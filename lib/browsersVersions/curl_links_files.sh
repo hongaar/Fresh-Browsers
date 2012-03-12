a000beeb816115aed78a7a20ea05a58d7c06eacd
Groozly <elfimov@gmail.com>
2012-03-12 22:31:48 +0400

    sh script

---
+++ lib/browsersVersions/curl_links_files.sh
@@ -0,0 +1,11 @@
+!\bin\sh
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_stable_software_release/Google_Chrome > chrome_stable.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_preview_software_release/Google_Chrome > chrome_preview.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_stable_software_release/Firefox > firefox_stable.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_preview_software_release/Firefox > firefox_preview.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_stable_software_release/Opera > opera_stable.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_preview_software_release/Opera > opera_preview.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_stable_software_release/Internet_Explorer > ie_stable.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_preview_software_release/Internet_Explorer > ie_preview.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_stable_software_release/Safari > safari_stable.txt
+curl http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_preview_software_release/Safari > safari_preview.txt
---
+++ tpl/createsh.php
@@ -0,0 +1,5 @@
+<?php
+
+$this->mainTemplate = 'empty.tpl';
+
+$this->lib->browsersVersions->createSh();
